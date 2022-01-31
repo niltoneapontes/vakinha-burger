b6e053f8ffe4ec4f23a9f300acb0dab3880022fb
Nilton <nilton.pontes@jbq.global>
2022-01-31 12:22:59 -0300

    feat: input field widget

--- lib/apps/modules/splash/splash_page.dart
+++ lib/apps/modules/splash/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vakinha_burger_mobile/apps/core/ui/widgets/vakinha_textformfield.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -9,7 +10,14 @@ class SplashPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Splash'),
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: const VakinhaTextformfield(label: 'Text Form'),
+          )
+        ],
+      ),
     );
   }
 }
