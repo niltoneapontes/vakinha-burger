0a372f3200f39b3e5f7a6863add670c7b47bc681
Nilton <nilton.pontes@jbq.global>
2022-01-31 12:01:01 -0300

    feat: initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:vakinha_burger_mobile/apps/core/ui/vakinha_ui.dart';
+import 'package:vakinha_burger_mobile/apps/modules/routes/splash_routers.dart';
+
+void main() {
+  runApp(const VakinhaBurgerMainApp());
+}
+
+class VakinhaBurgerMainApp extends StatelessWidget {
+  const VakinhaBurgerMainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Vakinha Burger',
+      theme: VakinhaUi.theme,
+      getPages: [...SplashRouters.routers],
+    );
+  }
+}
